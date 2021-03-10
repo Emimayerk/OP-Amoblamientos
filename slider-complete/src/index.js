75be61c04ff074ba878d1e72ffd50a300b25fe3f
Emiliano Mayer Viegas <emilianomayerviegas@gmail.com>
2021-03-10 13:26:07 -0300

    cambio de slider

--- slider-complete/src/index.js
+++
@@ -1,4 +0,0 @@
-import main from './main/index.js';
-
-window.customElements.define('slider-app', main.Slider);
-window.customElements.define('slide-app', main.Slide);
--- slider-complete/src/main/index.js
+++
@@ -1,9 +0,0 @@
-import Slider from './Slider/index.js';
-import Slide from './Slide/index.js';
-
-const main = {
-    Slider: Slider,
-    Slide: Slide
-}
-
-export { main as default };
